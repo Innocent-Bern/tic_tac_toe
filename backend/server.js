2772ce7573eb3924bed7eb77d8f27baef2ae585b
Innocent_Bern <innocentbern22@gmail.com>
2023-03-29 15:30:25 +0300

    Basic server setup

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server, {
+    cors : {
+        origin: "http://localhost:3000"
+    }
+});
+
+io.on('connection', (socket) => {
+    console.log("A user connected");
+});
+
+server.listen(8000, ()=> {
+    console.log("Listening on port 8000");
+});
