4ac9a91ddaa159993b181b7b138df2d0e1941596
Innocent_Bern <innocentbern22@gmail.com>
2023-03-29 19:21:10 +0300

    Basic board layout

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -1,9 +1,23 @@
 import styles from '@/styles/Home.module.css'
 import { Time } from '@/features/clock/Time'
-import { Suspense } from 'react'
+import { Suspense, useState } from 'react'
 
 export default function Home() {
+  const addTiles = ()=>{
+    let tiles = [];
+    let areas = "abcdefghi"
+    for (let i = 0; i < 9; i++){
+      tiles.push(<div style={{gridArea: areas[i]}} key={i} className={styles.board_tile}></div>)
+    }
+    return tiles;
+  }
+
   return (
-    <h1>Date <Suspense fallback={null}> <Time /></Suspense> </h1>
+    <div className={styles.Home}>
+      <h1>Date <Suspense fallback={null}> <Time /></Suspense> </h1>
+      <div className={styles.board} >
+        {addTiles().map((element)=> element)}
+      </div>
+    </div>
   )
 }
